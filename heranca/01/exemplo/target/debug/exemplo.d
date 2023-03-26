d34f9414fa058391a620e8d7344b4e49640209ba
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-03-26 03:48:56 -0300

    Adiciona o arquivo heranca/01/exemplo/.gitignore

--- heranca/01/exemplo/target/debug/exemplo.d
+++
@@ -1,1 +0,0 @@
-/workspace/estudo-rust/heranca/01/exemplo/target/debug/exemplo: /workspace/estudo-rust/heranca/01/exemplo/src/cachorro.rs /workspace/estudo-rust/heranca/01/exemplo/src/gato.rs /workspace/estudo-rust/heranca/01/exemplo/src/main.rs /workspace/estudo-rust/heranca/01/exemplo/src/mamifero.rs
